272eafc0babfd88a1cab0d562d1d98d5ecd2a676
rbngm <rabinghimire2012@gmail.com>
2023-02-02 11:41:58 +0800

    Change the name of a Playlist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,10 +3,20 @@ import { TrackList } from "../TrackList/TrackList";
 import "./Playlist.css";
 
 export class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    const playlist = e.target.value;
+    this.props.onNameChange(playlist);
+  }
+
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={"New Playlist"} />
+        <input defaultValue={"New Playlist"} onChange={this.handleNameChange} />
         <TrackList
           tracks={this.props.playlistTracks}
           onRemove={this.props.onRemove}
